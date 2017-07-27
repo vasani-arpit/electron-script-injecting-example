474afa6c7519ded187afa8840cf0ce86b43b1e33
vasani-arpit <arpit@technobrains.net>
2017-07-27 18:32:02 +0530

    bumping this

--- manipulateGoogle.js
+++ manipulateGoogle.js
@@ -1,3 +1,20 @@
+const {ipcRenderer} = require('electron');
+
+// Do something according to a request of your mainview
+ipcRenderer.on('request', function(){
+    ipcRenderer.sendToHost(getScripts());
+});
+
+ipcRenderer.on("alert-something",function(event,data){
+    alert(data);
+});
+
+ipcRenderer.on("change-text-element",function(event,data){
+    // the document references to the document of the <webview>
+    document.getElementById(data.id).innerHTML = data.text;
+});
+
+
 function addJQueryScript() {
   var script = document.createElement("script");
   script.src = "https://code.jquery.com/jquery-3.2.1.min.js";
@@ -15,3 +32,20 @@ let scriptAddInterval = setInterval(() => {
     clearInterval(scriptAddInterval);
   }
 });
+
+
+/**
+ * Simple function to return the source path of all the scripts in the document
+ * of the <webview>
+ *
+ *@returns {String}
+ **/
+function getScripts(){
+    var items = [];
+    
+    for(var i = 0;i < document.scripts.length;i++){
+        items.push(document.scripts[i].src);
+    }
+    
+    return JSON.stringify(items);
+}
